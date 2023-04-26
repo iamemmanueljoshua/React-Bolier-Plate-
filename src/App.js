54e40032a6a0ba801d9599359224b9dc7be9e09f
Emmanuel Joshua <iamemmanueljoshua@gmail.com>
2023-04-26 10:53:29 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const App = () => {
+  return (
+    <div>
+      This was made from Scratch! Also hello world!
+    </div>
+  );
+
+};
+
+export default App;
